6dbb4f4835747ae994ce3a17a301ca2a37a0698b
petrushka <petrushkin@yandex.ru>
2014-04-14 21:31:21 +0400

    New: add chat name to output

--- models/chat.rb
+++ models/chat.rb
@@ -2,4 +2,12 @@ class Chat < ActiveRecord::Base
   has_many :events, foreign_key: 'ChatID'
   self.primary_key = 'ChatID'
   self.table_name = 'chatinfo'
+
+  def display_name
+    if self.Name == 'Group'
+      self.Token
+    else
+      self.Name
+    end
+  end
 end
--- models/event.rb
+++ models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
-  has_one :message, foreign_key: "EventID"
-  belongs_to :chat
+  has_one :message, foreign_key: 'EventID'
+  belongs_to :chat, foreign_key: 'ChatID'
   belongs_to :contactrelation, foreign_key: 'Number'
   has_one :contact, through: :contactrelation
   self.primary_key = 'EventID'
