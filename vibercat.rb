1f49c1ba91a4a78c08a33fcd15e637730473281c
petrushka <petrushkin@yandex.ru>
2014-04-18 20:06:50 +0400

    Fix minute format

--- vibercat.rb
+++ vibercat.rb
@@ -2,7 +2,7 @@ require './setup.rb'
 
 Event.where('TimeStamp > ?', 1.month.ago.to_i).order(:TimeStamp).each do |e|
   begin
-    puts "[#{e.chat.display_name}] #{e.created_at.strftime('%d.%m.%Y %H:%m:%S')}, #{e.contact_nickname}: #{e.message.try(:Body)}"
+    puts "[#{e.chat.display_name}] #{e.created_at.strftime('%d.%m.%Y %H:%M:%S')}, #{e.contact_nickname}: #{e.message.try(:Body)}"
   rescue Errno::EPIPE
     break
   end
